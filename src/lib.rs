ee48ea4582321a5998ebed9b20fc459adefd92a4
Alexander Larin <scalar438@gmail.com>
2020-03-07 08:12:21 +0300

    Use CommandLineToArgvW, almost implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+extern crate libc;
 extern crate ntapi;
 extern crate winapi;
 
 use winapi::shared::minwindef::{DWORD, FALSE, TRUE};
 use winapi::um::handleapi::CloseHandle;
 use winapi::um::processthreadsapi::OpenProcess;
+use winapi::um::winbase::LocalFree;
 use winapi::um::winnt::HANDLE;
 use winapi::um::winnt::{PROCESS_QUERY_INFORMATION, PROCESS_VM_READ};
 
@@ -101,8 +103,10 @@ fn parse_command_line(s: &str) -> Vec<String> {
 pub fn get_cmd_line(pid: DWORD) -> Vec<String> {
 	use ntapi::ntpebteb::{PEB, PPEB};
 	use ntapi::ntrtl::{PRTL_USER_PROCESS_PARAMETERS, RTL_USER_PROCESS_PARAMETERS};
+	use std::mem::MaybeUninit;
 	use winapi::shared::basetsd::SIZE_T;
 	use winapi::um::memoryapi::ReadProcessMemory;
+	use winapi::um::shellapi::CommandLineToArgvW;
 
 	unsafe {
 		let handle = match get_process_handler(pid) {
@@ -112,7 +116,7 @@ pub fn get_cmd_line(pid: DWORD) -> Vec<String> {
 
 		let handle = handle.handle;
 
-		let mut pinfo = std::mem::MaybeUninit::<PROCESS_BASIC_INFORMATION>::uninit();
+		let mut pinfo = MaybeUninit::<PROCESS_BASIC_INFORMATION>::uninit();
 		if NtQueryInformationProcess(
 			handle,
 			0, // ProcessBasicInformation
@@ -126,7 +130,7 @@ pub fn get_cmd_line(pid: DWORD) -> Vec<String> {
 		let pinfo = pinfo.assume_init();
 
 		let ppeb: PPEB = pinfo.PebBaseAddress;
-		let mut peb_copy = std::mem::MaybeUninit::<PEB>::uninit();
+		let mut peb_copy = MaybeUninit::<PEB>::uninit();
 		if ReadProcessMemory(
 			handle,
 			ppeb as *mut _,
@@ -140,8 +144,7 @@ pub fn get_cmd_line(pid: DWORD) -> Vec<String> {
 		let peb_copy = peb_copy.assume_init();
 
 		let proc_param = peb_copy.ProcessParameters;
-		let mut rtl_proc_param_copy =
-			std::mem::MaybeUninit::<RTL_USER_PROCESS_PARAMETERS>::uninit();
+		let mut rtl_proc_param_copy = MaybeUninit::<RTL_USER_PROCESS_PARAMETERS>::uninit();
 		if ReadProcessMemory(
 			handle,
 			proc_param as *mut PRTL_USER_PROCESS_PARAMETERS as *mut _,
@@ -155,27 +158,37 @@ pub fn get_cmd_line(pid: DWORD) -> Vec<String> {
 		let rtl_proc_param_copy = rtl_proc_param_copy.assume_init();
 
 		let len = rtl_proc_param_copy.CommandLine.Length as usize;
-		if len % 2 == 1 {
-			// Just in case, I don't know can it happen or not
-			return Vec::new();
-		}
-		let len = len / 2;
-		let mut buffer_copy: Vec<u16> = Vec::with_capacity(len);
+		let mut buffer_copy: Vec<u8> = Vec::with_capacity(len);
 		buffer_copy.set_len(len);
 		if ReadProcessMemory(
 			handle,
 			rtl_proc_param_copy.CommandLine.Buffer as *mut _,
 			buffer_copy.as_mut_ptr() as *mut _,
-			len * 2 as SIZE_T,
+			len as SIZE_T,
 			null_mut(),
 		) != TRUE
 		{
 			return Vec::new();
 		}
 
-		let cmdline_full = String::from_utf16_lossy(&buffer_copy);
-		println!("Cmdline: {}", cmdline_full);
-		parse_command_line(&cmdline_full)
+		let mut argc = MaybeUninit::<i32>::uninit();
+		let argv_p = CommandLineToArgvW(buffer_copy.as_ptr() as *const _, argc.as_mut_ptr());
+		if argv_p.is_null() {
+			return Vec::new();
+		}
+		let argc = argc.assume_init();
+
+		let argv = std::slice::from_raw_parts(argv_p, argc as usize);
+		let mut res = Vec::new();
+		for arg in argv {
+			let len = libc::wcslen(*arg);
+			let str_slice = std::slice::from_raw_parts(*arg, len);
+			res.push(String::from_utf16_lossy(str_slice));
+		}
+
+		LocalFree(argv_p as *mut _);
+
+		res
 	}
 }
 
@@ -184,39 +197,6 @@ mod test {
 
 	use super::*;
 
-	#[test]
-	fn test_parse_cmdilne() {
-		assert_eq!(parse_command_line("a b"), vec!["a", "b"],);
-		assert_eq!(parse_command_line(r#"\"a\"     b"#), vec![r#""a""#, "b"]);
-
-		// With spaces
-		assert_eq!(parse_command_line(r#""a  b"  c"#), vec!["a  b", "c"]);
-
-		// With quotes
-		assert_eq!(parse_command_line(r#"a\"b  c"#), vec![r#"a"b"#, "c"]);
-
-		// With quotes, spaces and backslashes
-		assert_eq!(
-			parse_command_line(r#" "a \ \"b" \\  "\\ c\""#),
-			vec![r#"a \ "b"#, "\\\\", r#"\\ c""#]
-		);
-		assert_eq!(
-			parse_command_line(
-				r#"arg1 arg2\\with_backslash_without_space "arg3 with \"spaces \ and backslash" "#
-			),
-			vec![
-				"arg1",
-				r#"arg2\\with_backslash_without_space"#,
-				r#"arg3 with "spaces \ and backslash"#
-			]
-		);
-		assert_eq!(parse_command_line(r#"qwe q\"#), vec!["qwe", "q\\"]);
-		assert_eq!(parse_command_line(r#"qwe \\"\"#), vec!["qwe", "\\\"\\"]);
-		assert_eq!(
-			parse_command_line(r#""print_args" \" ' "\\\" \\\"""#),
-			vec!["print_args", "\"", "\'", "\\\" \\\""]
-		);
-	}
 	fn check(args: &[&str]) {
 		let mut command = std::process::Command::new("print_args");
 		let mut expected = vec!["print_args"]; // First arg is always in quotes
@@ -230,20 +210,47 @@ mod test {
 		let mut command = command.spawn().unwrap();
 		let cmdline = get_cmd_line(command.id());
 
-		// println!("{}", command);
-
 		assert_eq!(cmdline, expected);
 		command.wait().unwrap();
 	}
 
 	#[test]
 	fn test1() {
-		//	check(&["qwerty"]);
-		//	check(&["first arg with spaces", "second_arg"]);
-		//	check(&["first_arg_without_spaces", "second arg with spaces"]);
-		//	check(&["arg_with_\"quotes\""]);
-		//check(&["arg_with_\"quotes\" \\   and spaces"]);
-		//check(&["arg_with_\"backslash"]);
+		check(&["qwerty"]);
+	}
+
+	#[test]
+	fn test2() {
+		check(&["first arg with spaces", "second_arg"]);
+	}
+
+	#[test]
+	fn test3() {
+		check(&["first_arg_without_spaces", "second arg with spaces"]);
+	}
+
+	#[test]
+	fn test4() {
+		check(&["arg_with_\"quotes\""]);
+	}
+
+	#[test]
+	fn test5() {
+		check(&["arg_with_\"quotes\" \\   and spaces"]);
+	}
+
+	#[test]
+	fn test6() {
+		check(&["arg_with_\"backslash"]);
+	}
+
+	#[test]
+	fn test7() {
 		check(&["\"", "'", r#"\" \""#]);
+	}
+
+	#[test]
+	fn test8() {
+		check(&["\"", "'", r#"\\\" \""#]);
 	}
 }
