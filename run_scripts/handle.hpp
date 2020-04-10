59bce5284a50215abd63f778e0f1f7a2f53dbda8
Alexander Larin <scalar438@gmail.com>
2020-04-10 08:33:14 +0300

    Implementation in progress

---
+++ run_scripts/handle.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <Windows.h>
+
+struct Handle
+{
+	Handle() { m_handle = NULL; }
+
+	Handle(Handle &&old) noexcept : m_handle(old.m_handle) { old.m_handle = NULL; }
+
+	Handle(HANDLE h) : m_handle(h) {}
+
+	~Handle()
+	{
+		if (m_handle != NULL) CloseHandle(m_handle);
+	}
+
+	Handle(const Handle &) = delete;
+	Handle &operator=(const Handle &) = delete;
+
+	HANDLE m_handle;
+};
--- run_scripts/main.cpp
+++ run_scripts/main.cpp
@@ -2,9 +2,22 @@
 #include <iostream>
 #include <string_view>
 
-bool run_cross(std::string_view p1, std::string_view p2)
+#include "handle.hpp"
+
+// 0 - no error
+// 1 - program didn't return command line
+// 2 - something went wrong
+int run_cross(std::string_view p1, std::string_view p2)
 {
-	return true;
+	std::string cmdline1 = std::string(p1) + " " + " first_arg";
+	std::string cmdline2 = std::string(p2) + " " + " second_arg";
+	SECURITY_ATTRIBUTES sa;
+	sa.bInheritHandle = true;
+	sa.nLength = sizeof(sa);
+	sa.lpSecurityDescriptor = NULL;
+
+	//CreateProcessA(NULL, &cmdline1[0], &sa, &sa, true, )
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -13,10 +26,18 @@ int main(int argc, char *argv[])
 	{
 		for (int j = i; j <= argc; ++j)
 		{
-			if (!run_cross(argv[i], argv[j]))
+			switch (run_cross(argv[i], argv[j]))
 			{
-				std::cerr << "Something went wrong\n";
-				return -1;
+			case 0:
+				// All ok, do nothing
+				break;
+
+			case 1:
+				std::cerr << "Program didn't return command line\n";
+				std::cerr << "Pair of programs: " << argv[i] << " and " << argv[j] << '\n';
+				return 1;
+
+			case 2: std::cerr << "Something went wrong\n"; return -1;
 			}
 		}
 	}
