64311a9a75bcc445e0a2b551ed9bde34c751504b
Alexander Larin <scalar438@gmail.com>
2020-04-12 13:27:43 +0300

    Change rust testing program

--- rust_cmdline_getter/src/main.rs
+++ rust_cmdline_getter/src/main.rs
@@ -1,27 +1,9 @@
 extern crate sysinfo;
 use sysinfo::ProcessExt;
 
-fn main() -> std::io::Result<()> {
+fn main() {
 	let mut input = String::new();
-	loop {
-		input.clear();
-		println!("Enter pid (0 if you want to exit):");
-		std::io::stdin().read_line(&mut input)?;
-		let pid;
-		match input.trim().parse::<sysinfo::Pid>() {
-			Ok(r) => pid = r,
-			Err(_) => {
-				println!("Sorry, try again.");
-				continue;
-			}
-		}
-		if pid == 0 {
-			break;
-		}
-
-		let p = sysinfo::Process::new(pid, None, 0);
-		println!("Process cmdline with pid = {:} is {:?}", pid, p.cmd());
-	}
-
-	Ok(())
+	std::io::stdin().read_line(&mut input).unwrap();
+	print!("{}", sysinfo::Process::new(input.trim().parse().unwrap(), None, 0).cmd().join(" "));
+	std::thread::sleep(std::time::Duration::from_secs(1));
 }
