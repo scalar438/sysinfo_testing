7889a9a99e54cd1793b04a99114c8795588f0300
Alexander Larin <scalar438@gmail.com>
2020-05-03 13:23:50 +0300

    Remove warnings

--- rust_new_approach/src/main.rs
+++ rust_new_approach/src/main.rs
@@ -3,8 +3,8 @@ extern crate winapi;
 
 use std::mem::MaybeUninit;
 
-use winapi::shared::minwindef::{DWORD, FALSE, ULONG};
-use winapi::shared::ntdef::{HANDLE, NTSTATUS, UNICODE_STRING};
+use winapi::shared::minwindef::{FALSE, ULONG};
+use winapi::shared::ntdef::{HANDLE, NT_SUCCESS, UNICODE_STRING};
 use winapi::shared::ntstatus::{
 	STATUS_BUFFER_OVERFLOW, STATUS_BUFFER_TOO_SMALL, STATUS_INFO_LENGTH_MISMATCH,
 };
@@ -13,50 +13,48 @@ use winapi::um::winnt::PROCESS_QUERY_INFORMATION;
 
 use ntapi::ntpsapi::{NtQueryInformationProcess, ProcessCommandLineInformation, PROCESSINFOCLASS};
 
-unsafe fn create_unicode_string(p: *const UNICODE_STRING) -> String {
-	let slice = std::slice::from_raw_parts((*p).Buffer, (*p).Length as usize / 2);
-	String::from_utf16_lossy(slice)
-}
-
-unsafe fn PhpQueryProcessVariableSize(
-	ProcessHandle: HANDLE,
-	ProcessInformationClass: PROCESSINFOCLASS,
-) -> Result<Vec<String>, NTSTATUS> {
-	let mut returnLength: ULONG = 0;
+unsafe fn ph_query_process_variable_size(
+	process_handle: HANDLE,
+	process_information_class: PROCESSINFOCLASS,
+) -> Option<Vec<String>> {
+	let mut return_length: ULONG = 0;
 
 	let mut status = NtQueryInformationProcess(
-		ProcessHandle,
-		ProcessInformationClass,
+		process_handle,
+		process_information_class,
 		std::ptr::null_mut(),
 		0,
-		&mut returnLength as *mut _,
+		&mut return_length as *mut _,
 	);
 
-	if (status != STATUS_BUFFER_OVERFLOW
+	if status != STATUS_BUFFER_OVERFLOW
 		&& status != STATUS_BUFFER_TOO_SMALL
-		&& status != STATUS_INFO_LENGTH_MISMATCH)
+		&& status != STATUS_INFO_LENGTH_MISMATCH
 	{
-		return Err(status);
+		return None;
 	}
 
-	let buf_len = (returnLength as usize) / 2;
+	let buf_len = (return_length as usize) / 2;
 	let mut buffer: Vec<u16> = Vec::with_capacity(buf_len + 1);
 	buffer.set_len(buf_len);
 
 	status = NtQueryInformationProcess(
-		ProcessHandle,
-		ProcessInformationClass,
+		process_handle,
+		process_information_class,
 		buffer.as_mut_ptr() as *mut _,
-		returnLength,
-		&mut returnLength as *mut _,
+		return_length,
+		&mut return_length as *mut _,
 	);
+	if !NT_SUCCESS(status) {
+		return None;
+	}
 	let buffer = (*(buffer.as_ptr() as *const UNICODE_STRING)).Buffer;
 
 	// Get argc and argv from command line
 	let mut argc = MaybeUninit::<i32>::uninit();
 	let argv_p = winapi::um::shellapi::CommandLineToArgvW(buffer as *const _, argc.as_mut_ptr());
 	if argv_p.is_null() {
-		return Ok(Vec::new());
+		return Some(Vec::new());
 	}
 	let argc = argc.assume_init();
 	let argv = std::slice::from_raw_parts(argv_p, argc as usize);
@@ -69,11 +67,11 @@ unsafe fn PhpQueryProcessVariableSize(
 	}
 	winapi::um::winbase::LocalFree(argv_p as *mut _);
 
-	return Ok(res);
+	return Some(res);
 }
 
 fn get_cmd_line(h: HANDLE) -> Vec<String> {
-	if let Ok(v) = unsafe { PhpQueryProcessVariableSize(h, ProcessCommandLineInformation) } {
+	if let Some(v) = unsafe { ph_query_process_variable_size(h, ProcessCommandLineInformation) } {
 		v
 	} else {
 		vec![]
